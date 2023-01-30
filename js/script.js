1c101f433ba39736ac8194b82f6c2b6211dae03c
renatopl97 <renatopl97@gmail.com>
2023-01-30 00:01:19 -0300

    alteração na estrutura organizacional dos arquivos dos arquivos e criação do script inicial, selecionando o display e capturando os botões

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const operacaoAnteriorText = document.querySelector('.calculadora__display--anterior');
+const operacaoAtualText = document.querySelector('.calculadora__display--atual')
+const buttons = document.querySelectorAll('.calculadora__botoes button');
+
+console.log(buttons);
