be66ddbf738a262517b9b990fe901fd8e2a02094
renatopl97 <renatopl97@gmail.com>
2023-01-31 23:58:05 -0300

    calculadora praticamente finalizada, faltando apenas alguns ajustes

--- js/script.js
+++ js/script.js
@@ -22,16 +22,52 @@ class Calculadora {
 
 // Processa todos os cálculos
 processaOperacoes(operacoes) {
-    
+    //checagem se o valor atual está vazio
+    if(this.operacaoAtualText.innerText === "" && operacoes !== "C") {
+        //muda a operação
+        if(this.operacaoAnteriorText.innerText !== "") {
+            this.mudaOperacao(operacoes);
+        }
+        return;
+    }
+
     //pega valores atuais e anteriores
     let valorOperacao;
-    const anterior = +this.operacaoAnteriorText.innerText;
+    const anterior = +this.operacaoAnteriorText.innerText.split(" ")[0];
     const atual = +this.operacaoAtualText.innerText;
 
     switch(operacoes) {
         case "+":
             valorOperacao = anterior + atual;
             this.updateScreen(valorOperacao, operacoes, atual, anterior);
+            break;
+        case "-":
+            valorOperacao = anterior - atual;
+            this.updateScreen(valorOperacao, operacoes, atual, anterior);
+            break;
+        case "/":
+            valorOperacao = anterior / atual;
+            this.updateScreen(valorOperacao, operacoes, atual, anterior);
+            break;
+        case "*":
+            valorOperacao = anterior * atual;
+            this.updateScreen(valorOperacao, operacoes, atual, anterior);
+            break;
+        case "DEL":
+            valorOperacao = anterior * atual;
+            this.opeDel();
+            break;
+        case "CE":
+            valorOperacao = anterior * atual;
+            this.opeCe();
+            break;
+        case "C":
+            valorOperacao = anterior * atual;
+            this.opeC();
+            break;
+        case "=":
+            valorOperacao = anterior * atual;
+            this.opeIgual();
             break;
         default:
             return;
@@ -45,13 +81,49 @@ processaOperacoes(operacoes) {
         atual= null, 
         anterior = null
     ) {
-        console.log(valorOperacao, operacoes, atual, anterior);
-
         if(valorOperacao === null) {
             this.operacaoAtualText.innerText += this.operacaoAtual;
         } else {
-            
+            //checa se o valor é zero, se for apenas adiciona o valor
+            if(anterior === 0) {
+                valorOperacao = atual;
+            }
+
+            //adiciona o valor para o anterior
+            this.operacaoAnteriorText.innerText = `${valorOperacao} ${operacoes}`;
+            this.operacaoAtualText.innerText = "";
         }
+    }
+
+    // muda a operação matemática 
+    mudaOperacao(operacoes) {
+        const operacoesMath = ["*", "/", "-", "+"]
+
+        if(!operacoesMath.includes(operacoes)) {
+            return
+        }
+
+        this.operacaoAnteriorText.innerText = this.operacaoAnteriorText.innerText.slice(0, -1) + operacoes;
+    }
+
+    //deleta o ultimo digito
+    opeDel() {
+        this.operacaoAtualText.innerText = this.operacaoAtualText.innerText.slice(0, -1);
+    }
+
+    //limpa a operação atual
+    opeCe() {
+        this.operacaoAtualText.innerText = "";
+    }
+    //limpa todas as operações
+    opeC() {
+        this.operacaoAnteriorText.innerText = "";
+        this.operacaoAtualText.innerText = "";
+    }
+
+    opeIgual() {
+        const operacoes = operacaoAnteriorText.innerText.split (" ")[1];
+        this.processaOperacoes(operacoes);
     }
 }
 
